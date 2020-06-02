acf5f25347d6e993e9ce1b79f231ab569feb6cfd
tonyjward <tony@statcore.co.uk>
2020-06-02 16:14:19 +0100

    Get artile text from list of links

--- scraping/helper.py
+++ scraping/helper.py
@@ -7,6 +7,15 @@ Created on Mon Jun  1 15:12:47 2020
 
 import time
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+def get_driver(command_executor = "http://127.0.0.1:4444/wd/hub",
+                       desired_capabilities = DesiredCapabilities.FIREFOX):
+    'Instantiate a Selenium Webdriver'
+    
+    return webdriver.Remote(command_executor, desired_capabilities)
+
 
 def scroll_down(driver, pause_time = None, scroll_limit = None):
     '''
@@ -79,7 +88,50 @@ def get_links(driver, html_class = "button button--smaller button--chromeless u-
     print(f'{len(links)} links found')
     print(f'{len(unique_links)} of those were unique')
     
-    return unique_links
+    return list(unique_links)
+
+def get_article_text(links, driver, pause_time = None, article_limit = None):
+    '''
+    Gets article text from a series of html links
+    
+    Arguments:
+        links       : an list of html links
+        driver      : an object of class webdriver from the Selenium package
+        pause_time  : float - number of seconds to wait between articles
+        
+    Returns:
+        a list containing the article text for each link
+    '''        
+        
+    if len(links) == 0:
+        raise ValueError('You provided an empty list')
+    
+    text = []
+    i = 0
+
+    for link in links:
+        print(f'Article {i} of {len(links)}')
+        print(link)
+        
+        try:
+            driver.get(link)
+            outer_html = driver.execute_script("return document.documentElement.outerHTML")
+            soup = BeautifulSoup(outer_html, 'html.parser')
+            text.append(soup.get_text())
+        except Error as e:
+            print(f"We got an error for link at position {i}")
+            print(str(e))
+        finally:
+            if i == article_limit:
+                print(f'We reached the article limit of {article_limit}')
+                return text
+            i += 1
+        
+    assert len(links) == len(text)
+    
+    return text
+
+
     
     
     
--- scraping/scraping_medium.py
+++ scraping/scraping_medium.py
@@ -4,35 +4,48 @@ Created on Tue Jun  2 12:52:57 2020
 
 @author: tony
 """
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import time
 
-from scraping.helper import scroll_down, get_links
+import pickle
+
+from scraping.helper import scroll_down, get_links, get_article_text
+from scraping.helper import get_driver
 
 # specify url
 URL = 'https://medium.com/search?q=xgboost' # 'https://medium.com/search?q=%22drawing%20pin%22'
 
 # instantiate webdriver
-driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.FIREFOX)
+# driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.FIREFOX)
+driver = get_driver()
 
 # load webpage
 driver.get(URL)
 assert "Medium" in driver.page_source
 
 # page is loaded dynamically so we need to scroll to bottom first
-scroll_down(driver, pause_time = 2, scroll_limit = 5)
+scroll_down(driver, pause_time = 1, scroll_limit = 5)
+# scroll_down(driver, pause_time = 2)
 
 # get links to all articles 
 links = get_links(driver)
 links
-        
+
+# reinstantiate driver (not sure why this is needed again)
+driver = get_driver()
+
+text = get_article_text(links, driver, pause_time = 4, article_limit = 5)
+# text = get_article_text(links, driver, pause_time = 4)        
 
 
-
-
+#driver.get(links[0])
+#outer_html = driver.execute_script("return document.documentElement.outerHTML")
+#soup = BeautifulSoup(outer_html, 'html.parser')
+#text.append(soup.get_text())
 
 # -----------------------------------------------------------------------------
 # 5. save results
-import pickle
-pickle.dump(links, open("links.p", "wb"))
+
+pickle.dump(links, open("scraping/data/links3.p", "wb"))
+pickle.dump(text, open("scraping/data/text3.p", "wb"))
+
+links2 = pickle.load(open("scraping/data/links.p", "rb"))
+text2 = pickle.load(open("scraping/data/text.p", "rb"))

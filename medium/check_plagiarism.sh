0a3ff90245505b0740315d142362e9de1c5a0b06
tonyjward <tony@statcore.co.uk>
2020-06-15 12:50:30 +0000

    Allow user to specify where to save results

--- medium/check_plagiarism.sh
+++ medium/check_plagiarism.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# Example usage: ./check_plagiarism.sh '[random forest, xgboost]' data
+# See https://stackoverflow.com/a/32763171 for explanation
 
-python -m src.containment "$1"
-# python -m src.lcs "$1"
-# python -m src.feature_matrix "$1"
+python -m src.containment "$1" "$2"
+python -m src.lcs "$1" "$2"
+python -m src.feature_matrix "$1" "$2"
 
 
--- medium/scrape_data.sh
+++ medium/scrape_data.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# Example usage: ./scrape_data.sh '[random forest, xgboost]' data
+# See https://stackoverflow.com/a/32763171 for explanation
 
-python -m src.scrape "$1"
-python -m src.clean_data "$1"
-python -m src.manipulate_data "$1"
+python -m src.scrape "$1" "$2"
+python -m src.clean_data "$1" "$2"
+python -m src.manipulate_data "$1" "$2"
 
